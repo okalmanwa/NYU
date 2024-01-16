86d90ab9fc6e069ff414be83faba5336b8c29435
Carey Manwa <okal@Careys-MacBook-Air.local>
2024-01-16 03:49:45 +0300

    Initial test commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,38 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+
+document.getElementById('sidebarToggle').addEventListener('click', function () {
+    var targetElement = document.getElementById('page-content-wrapper');
+    targetElement.classList.toggle('correct-pos');
+});
+
+document.getElementById('sidebarToggle').addEventListener('click', function () {
+    var targetElement = document.getElementById('top-navbar');
+    targetElement.classList.toggle('reset-pos-navbar');
+});
+
